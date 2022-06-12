83c8ad8c4b77e793e921b7c8c8eb88dbcd7c1500
Konoshevskyi Denys <107351163+kRabUz@users.noreply.github.com>
2022-06-12 17:05:05 +0300

    Add files via upload

---
+++ Lab3_1/MyList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba3_1
+{
+    class MyList
+    {
+        public int Number { get; set; }
+        public int Id { get; set; }
+
+        public MyList(int number, int id)
+        {
+            Number = number;
+            Id = id;
+        }
+    }
+}
